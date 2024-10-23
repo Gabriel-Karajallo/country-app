749bb39c8372d65410755947908d6771ad3d66f2
Gabriel-Karajallo <c.gabrielkarajallo@gmail.com>
2024-10-23 12:39:33 +0200

    country-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
 
@@ -18,6 +19,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     BrowserModule,
     SharedModule,
+    HttpClientModule,
   ],
 
   providers: [],
